7c49241ca3a1eec440d6367f3acbf573ad8828a9
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2014-05-14 00:00:45 -0400

    change deploy

--- openshift_deploy.sh
+++ openshift_deploy.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-app_name="mblinds" 
+app_name="vblinds" 
 data_dir=$(rhc ssh $app_name 'echo $OPENSHIFT_DATA_DIR')
 echo "Openshift data dir: $data_dir"
-
-rm deployments/ROOT.war
 
 echo "Build WAR file for deploy manual deploy to openshift"
 mvn -Denv.OPENSHIFT_DATA_DIR=$data_dir clean package -Popenshift,tomcat
 
 echo "Stoping app"
-rhc app stop -a $app_name
+#rhc app stop -a $app_name
 
-#rhc scp $app_name upload deployments/ROOT.war app-root/runtime/dependencies/jbossas/deployments/ROOT.war
-rhc scp $app_name upload deployments/ROOT.war app-root/runtime/dependencies/jbossews/webapps/ROOT.war
+#rhc scp $app_name upload deployments/ROOT.war app-root/runtime/dependencies/jbossews/webapps/ROOT.war
 
 echo "Starting app"
-rhc app start -a $app_name
+#rhc app start -a $app_name
