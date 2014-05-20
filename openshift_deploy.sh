bcd0bf440c0f1000dd1d5e20c5ae85766b9e7917
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2014-05-20 19:15:32 +0400

    ch1

--- openshift_deploy.sh
+++ openshift_deploy.sh
@@ -7,13 +7,14 @@ echo "Openshift data dir: $data_dir"
 echo "Build WAR file for deploy manual deploy to openshift"
 mvn -Denv.OPENSHIFT_DATA_DIR=$data_dir clean package -Popenshift,tomcat
 
-#echo "Stoping app"
-#rhc app stop -a $app_name
-
-#rhc scp $app_name upload deployments/ROOT.war app-root/runtime/dependencies/jbossews/webapps/ROOT.war
+echo "Remove war"
+rhc ssh $app_name 'rm app-root/data/ROOT.war'
 
 echo "Upload war"
 rhc scp $app_name upload megion-blindsweb/target/ROOT.war app-root/data/ROOT.war
 
-#echo "Starting app"
-#rhc app start -a $app_name
+echo "Stoping app"
+rhc app stop -a $app_name
+
+echo "Starting app"
+rhc app start -a $app_name
